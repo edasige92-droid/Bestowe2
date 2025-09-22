72f25c3c0a7d066de5c4f889d5e5417c45d7ae63
edasige92-droid <edasige92@gmail.com>
2025-09-22 13:57:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Serve index.html
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "overlay.html"));
 });
 
 // NEW: Facebook comments route
