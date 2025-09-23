77d1dcaa6e78df5f615fe20f5407ed3479c7914b
edasige92-droid <edasige92@gmail.com>
2025-09-23 16:53:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static(__dirname + "/public"));
 // Poll comments from Facebook API
 async function fetchComments() {
   try {
-    const url = `https://graph.facebook.com/v21.0/$657216063676510/comments?fields=from,message,created_time&access_token=$EAAJoQm5bKFIBPpZBLNhVXZCXkVc4913wt1ly0A8P5bdwOJZCWemsPvrgewZAZC5Xm5NWNz3jQWnjXWY7bH4O5hmrjfNMoyQvF5ZAKuki7tZAWnUYxqmo8054gfZCFL5fZCuIRKTRfmxZC8XUO06Tay6BXHyXifG7UZAm8JG5YhyEgm2lqncOH7HYY8hDQHpWpgtfdZBSN7vsbPTX8z6c1sk7RE1osby6tA0DSlMFm3p5qjZC53ZCl0ZCUK47MIcxZC1lFejo`;
+    const url = `https://graph.facebook.com/v21.0/${VIDEO_ID}/comments?fields=from,message,created_time&access_token=${PAGE_ACCESS_TOKEN}`;
     const response = await fetch(url);
     const data = await response.json();
 
